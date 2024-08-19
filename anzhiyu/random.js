1415ff11186abfc88926bb8b6a957cbcf1d751a2
iceblock-top <2419584050@qq.com>
2024-08-20 00:14:45 +0800

    Site updated: 2024-08-20 00:14:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/19/hello-world/"];function toRandomPost(){
+var posts=["2024/08/19/hello-world/","2024/08/20/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
